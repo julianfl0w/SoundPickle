3ec9a5110fc69612bcbb37075e3e1935a1563254
Julian Loiacono <jcloiacon@gmail.com>
2024-02-14 16:59:45 -0700

    Update sound_pickle.py

--- SoundPickle/sound_pickle.py
+++ SoundPickle/sound_pickle.py
@@ -9,12 +9,10 @@ import librosa
 import copy 
 here = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(here, ".."))
-sinodeDir = os.path.join(here, "..", "..", "sinode")
+sinodeDir = os.path.join(here, "..", "..")
 sys.path.insert(0, sinodeDir)
 
-if not os.path.exists(sinodeDir):
-    raise Exception(f"{sinodeDir} does not exist!")
-import sinode.sinode as sinode
+import sinode.sinode.sinode as sinode
 import SoundPickle as sp
 import io
 from pydub import AudioSegment
